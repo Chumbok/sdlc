93654949df199ef9ee61627b29122ab8e34fbf0d
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2019-04-27 20:31:21 +0200

    commented out Disabling job dsl scripts security script.

---
+++ files/jenkins/init.groovy.d/DisableJobDslScriptSecurity.groovy
@@ -0,0 +1,7 @@
+//import javaposse.jobdsl.plugin.GlobalJobDslSecurityConfiguration
+//import jenkins.model.GlobalConfiguration
+//
+//println "Disabling job dsl scripts security."
+//
+//GlobalConfiguration.all().get(GlobalJobDslSecurityConfiguration.class).useScriptSecurity = false
+//GlobalConfiguration.all().get(GlobalJobDslSecurityConfiguration.class).save()
