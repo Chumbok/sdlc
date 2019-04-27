1d9425017e3e2d1b4904cc14b2f607eee6679758
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2019-04-28 00:16:27 +0200

    create jobs from job defs.

--- src/main/groovy/com/chumbok/sdlc/JobBuilder.groovy
+++ src/main/groovy/com/chumbok/sdlc/JobBuilder.groovy
@@ -4,7 +4,61 @@ package com.chumbok.sdlc
 @Grab('org.yaml:snakeyaml:1.17')
 import org.yaml.snakeyaml.Yaml
 
-def yaml = readYaml file:"job-definition.yaml"
-println yaml
+def jobDefsFileLocation = 'src/main/resources/job-definition.yaml'
+def jobDefs = new File("${jenkins.model.Jenkins.instance.getJob('JobBuilder').workspace}/" + jobDefsFileLocation).text
+
+def jobDefsYaml = new Yaml().load(jobDefs)
+
+jobDefsYaml['jobDefinitions'].each { jobDef ->
+
+    println "Creating job: ${jobDef.name} for ${jobDef.gitUrl}"
+
+    println "Creating Project Folder: ${jobDef.jenkinsFolder}"
+    folder(jobDef.jenkinsFolder) {
+    }
+
+    pipelineJob(jobDef.jenkinsFolder + "/" + jobDef.name) {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(jobDef.gitUrl + '.git')
+                        }
+                        branches('master', '**/feature*')
+                        scriptPath('Jenkinsfile')
+                    }
+                }
+            }
+        }
+    }
+    println "Pipeline job: ${jobDef.jenkinsFolder}/${jobDef.name} created successfully."
+
+    multibranchPipelineJob(jobDef.jenkinsFolder + "/" + jobDef.name + "-ci") {
+
+        description("On SCM: <a href=\"${jobDef.gitUrl}\">${jobDef.gitUrl}</a>")
+
+        triggers {
+            cron('H H * * *')
+        }
+
+        branchSources {
+            github {
+                id("repo-${jobDef.name}")
+                repoOwner(jobDef.name)
+                repository(jobDef.name)
+            }
+        }
+
+        orphanedItemStrategy {
+            discardOldItems {
+                daysToKeep(1)  // remove merged pipelines every day
+            }
+        }
+    }
+
+    println "Multibranch Pipeline job: ${jobDef.jenkinsFolder}/${jobDef.name} created successfully."
+
+}
 
 
