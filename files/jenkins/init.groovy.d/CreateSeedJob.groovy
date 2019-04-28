e1e5afa29a7736abb871c7fd25e9b125165c60b3
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2019-04-28 14:01:02 +0200

    add auto job builder job

--- files/jenkins/init.groovy.d/CreateSeedJob.groovy
+++ files/jenkins/init.groovy.d/CreateSeedJob.groovy
@@ -1,9 +1,13 @@
 import jenkins.model.Jenkins
 
 println "Creating seed job from /usr/share/jenkins/JobBuilderJenkinsConfig.xml"
-
 new File("/usr/share/jenkins/JobBuilderJenkinsConfig.xml").withInputStream { stream ->
     Jenkins.instance.createProjectFromXML("JobBuilder", stream)
+}
+
+println "Creating seed job from /usr/share/jenkins/AutoJobBuilderJenkinsConfig.xml"
+new File("/usr/share/jenkins/AutoJobBuilderJenkinsConfig.xml").withInputStream { stream ->
+    Jenkins.instance.createProjectFromXML("AutoJobBuilder", stream)
 }
 
 println "Creating seed job complete."
